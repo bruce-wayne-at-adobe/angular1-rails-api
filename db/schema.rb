0066d01bd84cece6d32437d289e41c056ea79a6a
Wayne Banks <wbanks@adobe.com>
2016-11-21 01:13:40 -0800

    edited emotion view

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116223332) do
+ActiveRecord::Schema.define(version: 20161118231553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "azure_emos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "demos", force: :cascade do |t|
     t.string   "title"
